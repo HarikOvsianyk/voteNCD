b0507dba9415baf27fb109df92b95e4cb8af99c3
Harik Ovsianyk <ovsianyk@cp3258-lx>
2022-06-20 18:49:59 +0300

    Create a few new methods in contract

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,1 +1,22 @@
-import { logging, PersistentMap } from 'near-sdk-as'
+import { logging, PersistentMap } from "near-sdk-as";
+
+const VoteURL = new PersistentMap<string, string>("VoteURL");
+const VoteArray = new PersistentMap<string, i32[]>("Stores votes");
+const userParticipation = new PersistentMap<string, string[]>(
+  "user participation record"
+);
+
+//View methods
+export function getUrl(name: string): string {
+  if (VoteURL.contains(name)) {
+    return VoteURL.getSome(name);
+  }
+  logging.log('can not find that user');
+  return "";
+}
+
+//Change methods
+export function addUrl(name: string, url: string): void {
+  VoteURL.set(name, url);
+  logging.log(`Added url for ${name}`);
+}
