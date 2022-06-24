dc0da80a7b1bf1895c589985681bbf361c6f00a0
Harik Ovsianyk <ovsianyk@cp3258-lx>
2022-06-24 18:41:13 +0300

    Add spinner to lawPage

--- frontend/pages/lawPage/lawPage.tsx
+++ frontend/pages/lawPage/lawPage.tsx
@@ -1,6 +1,6 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useEffect, useState, useContext } from "react";
 import { Box, Paper, Typography } from "@mui/material";
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 import {
   LawWrapper,
   LawTitle,
@@ -8,70 +8,97 @@ import {
   LawExpDate,
   WrapperVoices,
 } from "./lawPageComponents";
-import { Button } from "../../components";
+import { Button, Spinner } from "../../components";
 import { IVoteProps } from "../../interfaces/interfaces";
-
+import { Context } from "../../context/context";
 
 export const LawPage: FunctionComponent = ({}) => {
+  const { spinner, spinnerOn, spinnerOff } = useContext(Context);
   const { id } = useParams();
-  const [{ lawTitle,lawName,author, expirationDate, forVote, against, description  }, setVote] = useState<IVoteProps>({
-    lawTitle: '',
-    author: '',
-    lawName: '',
-    expirationDate: '',
-    forVote: '',
-    against: '',
-    description: '',
-    id: '',
-  })
+  const [
+    {
+      lawTitle,
+      lawName,
+      author,
+      expirationDate,
+      forVote,
+      against,
+      description,
+    },
+    setVote,
+  ] = useState<IVoteProps>({
+    lawTitle: "",
+    author: "",
+    lawName: "",
+    expirationDate: "",
+    forVote: "",
+    against: "",
+    description: "",
+    id: "",
+  });
+  const getVote = async () => {
+    const vote = await window.contract.getVote({ id: id });
+    setVote(vote);
+    spinnerOff?.();
+  };
   useEffect(() => {
-    const getVote = async () => {
-      setVote(await window.contract.getVote({id: id}));
-    };
+    spinnerOn?.();
     getVote();
   }, []);
   return (
     <LawWrapper>
       <Paper elevation={3} sx={{ width: "500px" }}>
-      <Box
+        {spinner ? (
+          <Box             sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            p: 1,
+          }}>
+            <Spinner/>
+          </Box>
+        ) : (
+          <Box
             key={id}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              p: 1,
+            }}
+          >
+            <LawTitle>{lawTitle}</LawTitle>
+            <LawName>{lawName}</LawName>
+            <LawTitle>{author}</LawTitle>
+            <LawExpDate>Until: {expirationDate}</LawExpDate>
+            <Box
               sx={{
+                width: "100%",
                 display: "flex",
-                flexDirection: "column",
+                flexDirection: "row",
                 justifyContent: "space-between",
-                alignItems: "center",
-                p: 1,
               }}
             >
-              <LawTitle>{lawTitle}</LawTitle>
-              <LawName>{lawName}</LawName>
-              <LawTitle>{author}</LawTitle>
-              <LawExpDate>Until: {expirationDate}</LawExpDate>
-              <Box
-                sx={{
-                  width: '100%',
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <WrapperVoices>
-                  <Box sx={{ textAlign: "center" }}>
-                    For:{" "}
-                    <Typography sx={{ color: "green" }}>{forVote}</Typography>
-                  </Box>
-                  <Button>For</Button>
-                </WrapperVoices>
-                <WrapperVoices>
-                  <Box sx={{ textAlign: "center" }}>
-                    Against:{" "}
-                    <Typography sx={{ color: "red" }}>{against}</Typography>
-                  </Box>
-                  <Button>Against</Button>
-                </WrapperVoices>
-              </Box>
-              <Box sx={{ m: 2, textAlign: "justify" }}>{description}</Box>
+              <WrapperVoices>
+                <Box sx={{ textAlign: "center" }}>
+                  For:{" "}
+                  <Typography sx={{ color: "green" }}>{forVote}</Typography>
+                </Box>
+                <Button>For</Button>
+              </WrapperVoices>
+              <WrapperVoices>
+                <Box sx={{ textAlign: "center" }}>
+                  Against:{" "}
+                  <Typography sx={{ color: "red" }}>{against}</Typography>
+                </Box>
+                <Button>Against</Button>
+              </WrapperVoices>
             </Box>
+            <Box sx={{ m: 2, textAlign: "justify" }}>{description}</Box>
+          </Box>
+        )}
       </Paper>
     </LawWrapper>
   );
