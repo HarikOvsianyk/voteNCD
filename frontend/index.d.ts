a543da821abdc88393b8b67c1113a98d1024f3d0
Harik Ovsianyk <ovsianyk@cp3258-lx>
2022-06-29 13:48:27 +0300

    Update index.d.ts file

--- frontend/index.d.ts
+++ frontend/index.d.ts
@@ -1,4 +1,6 @@
-export {};
+import { WalletConnection } from "near-api-js";
+import { IVoteProps } from "./interfaces/interfaces";
+
 declare module "*.png" {
   const value: any;
   export = value;
@@ -6,10 +8,17 @@ declare module "*.png" {
 
 declare global {
   interface Window {
-    nearInitPromise: Promise<void>;
     accountId: string;
-    walletConnection: {
-      isSignedIn: () => boolean;
+    walletConnection: WalletConnection;
+    contract: {
+      getAllVotes: () => Promise<IVoteProps[]>;
+      getVote: ({ id: string }) => Promise<IVoteProps>;
+      isUserVoted: ({ id: string, accountId: string }) => Promise<bool>;
+      addToVotesMap: ({ vote: IVoteProps }) => Promise<void>;
+      addToParticipation: ({ id: string, accountId: string }) => Promise<void>;
+      addForVote: ({ id: string }) => Promise<void>;
+      addToAgainst: ({ id: string }) => Promise<void>;
+      clearVotesMAP: ({ id: string }) => Promise<void>;
     };
   }
 }
